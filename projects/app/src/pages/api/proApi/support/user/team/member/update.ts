09072354e272825a8a79e8ce452601d0e0829517
gujie <704572066@qq.com>
2024-04-16 12:05:32 +0800

    添加团队身份切换接口

---
+++ projects/app/src/pages/api/proApi/support/user/team/member/update.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { jsonRes } from '@fastgpt/service/common/response';
+import { connectToDatabase } from '@/service/mongo';
+import { MongoUser } from '@fastgpt/service/support/user/schema';
+import { MongoTeamMember } from '@fastgpt/service/support/user/team/teamMemberSchema';
+import { UpdateTeamMemberProps } from '@fastgpt/global/support/user/team/controller';
+/*  */
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await connectToDatabase();
+
+    const { memberId, teamId, role } = req.body as UpdateTeamMemberProps;
+
+    const tmb = await MongoTeamMember.findById(memberId);
+    if (!tmb) {
+      throw new Error('can not find it');
+    }
+
+    // 更新对应的记录
+    await MongoTeamMember.updateOne(
+      {
+        _id: memberId
+      },
+      {
+        role
+      }
+    );
+
+    jsonRes(res);
+  } catch (err) {
+    jsonRes(res, {
+      code: 500,
+      error: err
+    });
+  }
+}
